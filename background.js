5fddf5590ac9670c4d4e56f339970f40304da360
Vedhas <vedhaspatil@gmail.com>
2024-07-22 09:36:11 +0530

    working changes in manifest 2

--- background.js
+++ background.js
@@ -1,30 +1,22 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: [] // Empty array if no rules to remove
-  });
-
-  const blockedSites = [
-   "instagram",
+// List of websites to redirect
+const blockedSites = [
+    "instagram",
     "yandex",
     "dzen",
     ".ru"
   ];
-
-  const rules = blockedSites.map((site, index) => ({
-    id: index + 1, // Ensure the ID is an integer
-    priority: 100,
-    action: {
-      type: "redirect",
-      redirect: {
-        regexSubstitution: "https://forum.nofap.com/index.php?threads/how-do-you-fight-cravings-to-pmo.311524/"
+  
+  chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+      if (details.type === "main_frame") {
+        for (let site of blockedSites) {
+          if (details.url.includes(site)) {
+            return { redirectUrl: "https://forum.nofap.com/index.php?threads/how-do-you-fight-cravings-to-pmo.311524/" };
+          }
+        }
       }
     },
-    condition: {
-      regexFilter: `.*${site}.*`, // Ensure the regex matches the full URL
-      resourceTypes: ["main_frame"]
-    }
-  }));
-
-  chrome.declarativeNetRequest.updateDynamicRules({ removeRuleIds: [], addRules: rules });
-});
+    { urls: ["<all_urls>"] },
+    ["blocking"]
+  );
   
